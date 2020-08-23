da2dfa0ca716d95a00c379c8fb58a38c19984d5d
Reid Smith <smithreid001@gmail.com>
2020-08-22 23:51:30 -0400

    maybe finished parser, TODO system tick

--- parser.py
+++ parser.py
@@ -4,13 +4,8 @@ class Parser(object):
     def __init__(self, filename):
         with open(filename) as f:
             lines = f.readlines()
-        # TODO: split each line into requests
-        # TODO: split each request into a tuple (origin, destination)
-
+        self.instructions = [[[int(element) for element in request.split(' ')] for request in line.split(',')] for line in lines]
 
     def get_line(self):
-        '''
-        Parses a line and yields request objects
-        '''
-        for request in self.instructions[0]:
-            yield Request(request)
+        for line in self.instructions:
+            yield line
--- system.py
+++ system.py
@@ -5,16 +5,25 @@ class System(object):
         self.parser = parser
         self.n_floors = n_floors
         self.elevators = []
-        for _ in n_elevators: 
-            elevators.append(Elevator(self.n_floors / 2))
+        for _ in range(n_elevators): 
+            self.elevators.append(Elevator(self.n_floors / 2))
         self.active_requests = []
 
 
-    def tick(self):
-        self.parser.get_line()
+    def tick(self, line):
+        for request in line:
+            import pdb
+            pdb.set_trace()
         '''
-        TODO: retrieve a tick of instructions from the parser
+        TODO: find the closest elevator going in the right direction
+        TODO: assign request to that elevator
         TODO: determine which elevator to pass each request to and pass them
         TODO: move elevators, open doors, and close doors as appropriate
         TODO: check each active request and clear as appropriate
         '''
+
+    def run(self):
+        for line in self.parser.get_line():
+            import pdb
+            pdb.set_trace()
+            self.tick(line)
